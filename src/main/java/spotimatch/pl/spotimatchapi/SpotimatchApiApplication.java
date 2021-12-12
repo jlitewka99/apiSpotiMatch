3f375829df70a5b8d0fa276dfd0a538568c69b33
sh4 <7331319+ShamerOC@users.noreply.github.com>
2021-12-12 12:22:09 +0100

    init

---
+++ src/main/java/spotimatch/pl/spotimatchapi/SpotimatchApiApplication.java
@@ -0,0 +1,13 @@
+package spotimatch.pl.spotimatchapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotimatchApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotimatchApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spotimatch/pl/spotimatchapi/SpotimatchApiApplicationTests.java
@@ -0,0 +1,13 @@
+package spotimatch.pl.spotimatchapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotimatchApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
