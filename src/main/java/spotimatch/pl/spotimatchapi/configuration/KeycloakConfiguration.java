1a53e193473ed71a893ca96203a4e5c52142c2fe
J.Malkowski <jak.malkowski@gmail.com>
2022-01-17 12:39:11 +0100

    Fixed Cors configuration

--- src/main/java/spotimatch/pl/spotimatchapi/configuration/KeycloakConfiguration.java
+++ src/main/java/spotimatch/pl/spotimatchapi/configuration/KeycloakConfiguration.java
@@ -1,33 +1,14 @@
 package spotimatch.pl.spotimatchapi.configuration;
 
-//
-//@org.keycloak.adapters.springsecurity.KeycloakConfiguration
-//public class KeycloakConfiguration extends KeycloakWebSecurityConfigurerAdapter {
-//    @Autowired
-//    public void configureGlobal(
-//            AuthenticationManagerBuilder auth) throws Exception {
-//
-//        KeycloakAuthenticationProvider keycloakAuthenticationProvider
-//                = keycloakAuthenticationProvider();
-//        keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(
-//                new SimpleAuthorityMapper());
-//        auth.authenticationProvider(keycloakAuthenticationProvider);
-//    }
-//
-//    @Bean
-//    @Override
-//    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
-//        return new RegisterSessionAuthenticationStrategy(
-//                new SessionRegistryImpl());
-//    }
-//
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        super.configure(http);
-//        http.authorizeRequests()
-//                .antMatchers("/test*")
-//                .hasRole("user")
-//                .anyRequest()
-//                .permitAll();
-//    }
-//}
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KeycloakConfiguration {
+
+    @Bean
+    public KeycloakSpringBootConfigResolver KeycloakConfigResolver(){
+        return new KeycloakSpringBootConfigResolver();
+    }
+}
