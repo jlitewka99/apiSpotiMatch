c64c70c8d558a26ea25c8c33c5f1116bd3b18447
sh4 <7331319+ShamerOC@users.noreply.github.com>
2022-04-08 15:51:43 +0200

    sh4-dev: Fixed Maven issues

--- src/main/java/tk/spotimatch/api/controller/SampleController.java
+++ src/main/java/tk/spotimatch/api/controller/SampleController.java
@@ -1,25 +1,11 @@
 package tk.spotimatch.api.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import spotimatch.pl.spotimatchapi.model.TestModel;
 
-import java.security.Principal;
 
 @RestController
 public class SampleController {
-
-    @GetMapping("/something")
-    public TestModel something() {
-        return new TestModel("123", "abc");
-    }
-
-    @CrossOrigin(origins = "*", allowedHeaders = "*")
-    @GetMapping("/test")
-    public TestModel test(Principal principal) {
-        return new TestModel("123", "abc");
-    }
 
     @GetMapping("/")
     public String index() {
